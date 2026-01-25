e5386cb375ac31d206d0abc8682296555c977916
Raj Prabhu R <raj.prabhu@thoughtscrest.com>
2026-01-25 23:02:54 +0530

    [25-01-2026] Threads

---
+++ 20. Threads/MultiThreading.java
@@ -0,0 +1,33 @@
+package com.java.learning.threads;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class MultiThreading {
+
+	public static void main(String[] args) {
+		System.out.println("Multi-Threading");
+
+		MyThread example1 = new MyThread();
+		example1.start(); // starts the thread
+
+		Thread example2 = new Thread(new MyRunnable());
+		example2.start();
+	}
+
+}
+
+// Example:1:run
+//Thread  implements Runnable 
+class MyThread extends Thread {
+	public void run() {
+		System.out.println("Thread is running");
+	}
+}
+
+// Example:2:
+class MyRunnable implements Runnable {
+	public void run() {
+		System.out.println("Runnable thread is running");
+	}
+}
